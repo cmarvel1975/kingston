9489550b76e5b3a8295459b9dd0afb35181719db
kingstoncash <37273773+kingstoncash@users.noreply.github.com>
2018-03-17 11:33:48 +0100

    First Commit

--- src/compat/strnlen.cpp
+++ src/compat/strnlen.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "config/dixicoin-config.h"
+#include "config/kingston-config.h"
 #endif
 
 #include <cstring>
