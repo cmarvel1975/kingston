ebab6f86de611abe62bfe33aa894e3e3a7c72aa9
kingstoncash <37273773+kingstoncash@users.noreply.github.com>
2018-03-18 15:44:14 +0100

    Copyright Addition

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -2,7 +2,8 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2017 The PIVX developers
-// Copyright (c) 2017-2018 The Kingston developers
+// Copyright (c) 2017-2018 The DixiCoin developers
+// Copyright (c) 2018 The Kingston developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include "chainparams.h"
@@ -51,7 +52,7 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
     
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1521117500, // * UNIX timestamp of last checkpoint block
+    1519956858, // * UNIX timestamp of last checkpoint block
     0,    // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     2000        // * estimated number of transactions per day after checkpoint
@@ -99,7 +100,7 @@ public:
         nModifierUpdateBlock = 1;
 	
 
-	const char* pszTimestamp = "Dixicoin Official Release";
+	const char* pszTimestamp = "DixiCoin Official Release";
         CMutableTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
@@ -117,7 +118,9 @@ public:
         assert(hashGenesisBlock == uint256("0x000004c24f071662fe3cfbd5cd88976411a1eaafef20c03852264f6aa97de571"));
         assert(genesis.hashMerkleRoot == uint256("0x051012b014cd7c6b3390df44131176e8fb90b1683b354efb60f8f402d3779576"));
 	
-        vSeeds.push_back(CDNSSeedData("1", "81.169.179.241"));
+        vSeeds.push_back(CDNSSeedData("45.64.254.58","113.212.115.58"));
+	vSeeds.push_back(CDNSSeedData("103.19.211.58","81.169.179.241"));
+	vSeeds.push_back(CDNSSeedData("113.212.124.58","45.64.254.42"));
 		
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 46);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 90);
@@ -176,7 +179,7 @@ public:
         //assert(hashGenesisBlock == uint256("0x0000041e482b9b9691d98eefb48473405c0b8ec31b76df3797c74a78680ef818"));
         vFixedSeeds.clear();
         vSeeds.clear();
-        vSeeds.push_back(CDNSSeedData("1", "81.169.179.241"));
+        vSeeds.push_back(CDNSSeedData("", ""));
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 107); // Testnet kingston addresses start with 'x' or 'y'
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 19);  // Testnet kingston script addresses start with '8' or '9'
         base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 142);     // Testnet private keys start with '9' or 'c' (Bitcoin defaults)
@@ -225,12 +228,12 @@ public:
         nTargetTimespan = 24 * 60 * 60; // Kingston: 1 day
         nTargetSpacing = 1 * 60;        // Kingston: 1 minutes
         bnProofOfWorkLimit = ~uint256(0) >> 1;
-        genesis.nTime = 1521117400;
+        genesis.nTime = 1454124731;
         genesis.nBits = 0x207fffff;
         genesis.nNonce = 12345;
         hashGenesisBlock = genesis.GetHash();
         nDefaultPort = 9215;
-        //assert(hashGenesisBlock == uint256("0x00000774d9ca930a115f44612b9bf2990639b847bb9b56bbe72ba98adc85acb7"));
+        //assert(hashGenesisBlock == uint256("0x4f023a2120d9127b21bbad01724fdb79b519f593f2a85b60d3d79160ec5f29df"));
         vFixedSeeds.clear(); //! Testnet mode doesn't have any fixed seeds.
         vSeeds.clear();      //! Testnet mode doesn't have any DNS seeds.
         fRequireRPCPassword = false;
